2f0599d769943f23d96ba8d2e2c0db643ca8bbfa
mjoh223 <matthew.johnson@ucsf.edu>
2020-04-10 14:44:08 -0400

    add external cyto layout: cose-bilkent

--- dashapp.py
+++ dashapp.py
@@ -339,7 +339,7 @@ app.layout = html.Div([
                     id='cytoscape',
                     elements=default_elements,
                     style={'width': '100%', 'height': '800px'},
-                    layout={'name':'euler', 'animate':'True'},)
+                    layout={'name':'cose-bilkent', 'animate':'True'},)
         ],className = 'eight columns')
     ],className="row"),
 
